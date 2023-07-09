9c32ee81314436ec8a0042e21ea104cf474a27e4
Ditik Rimal <rml.ditik@gmal.com>
2023-07-09 07:49:28 +0545

    Image and Others

--- JS/main.js
+++ JS/main.js
@@ -1,40 +1,8 @@
-const inputs = document.querySelectorAll(".input-field");
-const toggle_btn = document.querySelectorAll(".toggle");
-const main = document.querySelector("main");
-const toggle_btn_out = document.querySelectorAll(".outToggle");
-const bullets = document.querySelectorAll(".bullets span");
-const images = document.querySelectorAll(".image");
-const bgImg = document.getElementById("bgimg");
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".header");
 
-inputs.forEach((inp) => {
-  inp.addEventListener("focus", () => {
-    inp.classList.add("active");
-  });
-  inp.addEventListener("blur", () => {
-    if (inp.value != "") return;
-    inp.classList.remove("active");
-  });
-});
+const toggleNavbar = () => {
+  nav_header.classList.toggle("active");
+};
 
-toggle_btn.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    main.classList.toggle("sign-up-mode");
-  });
-});
-
-toggle_btn_out.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    window.location = "index.php";
-    main.classList.toggle("sign-up-mode");
-  });
-});
-const body = document.querySelector(".body");
-const hamburger = document.querySelector(".hamburger");
-const menu = document.querySelector(".menu");
-const mblMenu = document.querySelector(".mobileNav");
-hamburger.addEventListener("click", function () {
-  this.classList.toggle("is-active");
-  bgImg.classList.toggle("bgimg-navActive");
-  mblMenu.classList.toggle("is-active");
-  hamburger.classList.toggle("span");
-});
+mobile_nav.addEventListener("click", () => toggleNavbar());

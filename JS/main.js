34a70514b06ffc38e194e50e896a4d2fdc9a65ec
Ditik Rimal <rml.ditik@gmal.com>
2023-07-09 15:23:08 +0545

    Made a Contact Box and basic styling

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,14 @@
 const mobile_nav = document.querySelector(".mobile-navbar-btn");
 const nav_header = document.querySelector(".header");
-
+const contactBtn = document.querySelector(".contactBtn");
+const contactBox = document.querySelector(".contactBox");
+const closeBtn = document.querySelector(".closeBtnDiv");
+contactBtn.addEventListener("click", () => {
+  contactBox.classList.toggle("active");
+});
+closeBtn.addEventListener("click", () => {
+  contactBox.classList.toggle("active");
+});
 const toggleNavbar = () => {
   nav_header.classList.toggle("active");
 };

43ec83642a7f3fc29c9e6c828f56ddeaff4a1ea7
Ditik Rimal <rml.ditik@gmal.com>
2023-07-11 23:04:32 +0545

    Sticky Navbar with Glass Looks

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,10 @@
 const mobile_nav = document.querySelector(".mobile-navbar-btn");
 const nav_header = document.querySelector(".header");
+
 const contactBtn = document.querySelector(".contactBtn");
+const mblContactBtn = document.getElementById("mblContactBtn");
+const navBtns = document.querySelector(".navbar-link");
+const mainContent = document.querySelector(".mainContent");
 const contactBox = document.querySelector(".contactBox");
 const closeBtn = document.querySelector(".closeBtnDiv");
 contactBtn.addEventListener("click", () => {
@@ -9,8 +13,17 @@ contactBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   contactBox.classList.toggle("active");
 });
+
 const toggleNavbar = () => {
   nav_header.classList.toggle("active");
 };
+const scrollHeader = () => {};
+// window.addEventListener("scroll", function () {
+//   nav_header.classList.add("scroll", window.scrollY > 100);
+//   mainContent.classList.add("scroll", window.scrollY > 100);
+// });
 
 mobile_nav.addEventListener("click", () => toggleNavbar());
+mblContactBtn.addEventListener("click", () => {
+  contactBox.classList.toggle("active");
+});
